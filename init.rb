c01b99558fd650a259a0f1e654e4921e3c4090c9
Leander Kretschmer <leanderkretschmer@icloud.com>
2026-07-02 10:04:03 +0000

    chore: add url/author_url metadata, bump version to 0.0.2

--- init.rb
+++ init.rb
@@ -3,9 +3,11 @@
 require_relative 'lib/alternating_journals/hooks'
 
 Redmine::Plugin.register :redmine_alternating_journals do
-  name        'Alternating Journals'
+  name        'Redmine Alternating Journals'
   author      'Leander Kretschmer'
+  author_url  'https://github.com/leanderkretschmer'
   description 'Wechselnde Hintergrundfarben für Kommentare/Journals auf Ticket-Seiten'
-  version     '1.0.0'
+  version     '0.0.2'
+  url         'https://github.com/leanderkretschmer/redmine_alternating_journals'
   requires_redmine version_or_higher: '6.0.0'
 end
